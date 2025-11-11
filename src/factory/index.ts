8dc035d2a7fc6b4e43eb103e2d21f388175c6e4e
VesperQuartz <lilbrown3000@gmail.com>
2025-11-11 08:15:13 +0100

    factory

---
+++ src/factory/index.ts
@@ -0,0 +1,17 @@
+import { createFactory } from "hono/factory";
+import type { auth } from "@/lib/auth";
+
+export const factory = createFactory<{
+	Variables: {
+		user: typeof auth.$Infer.Session.user | null;
+		session: typeof auth.$Infer.Session.session | null;
+	};
+}>();
+
+export const authMiddleware = factory.createMiddleware(async (c, next) => {
+	const session = c.get("session");
+	if (!session) {
+		return c.json({ message: "Unauthorized" }, 401);
+	}
+	return next();
+});
